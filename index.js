4b8b3990ea2f231f8cd224c4559ed5ca1ac26bc5
Poonam251295 <108549378+Poonam251295@users.noreply.github.com>
2025-12-16 14:57:22 +0000

    latest changes

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const { chromium } = require('playwright');
 
 (async () => {
-  const browser = await chromium.launch();
+  try {
+    const browser = await chromium.launch({ headless: true });
   const page = await browser.newPage();
-  await page.goto('https://www.google.com');
+
+  await page.goto('https://playwright.dev/docs/intro');
   console.log(await page.title());
-  await browser.close();
-  console.log("poonam")
+
+  await page.locator('.DocSearch-Button').click()
+  await page.locator('#docsearch-input').fill("writing tests")
+
+  await page.keyboard.press('Enter')
+
+  setTimeout(async () => {
+    await page.screenshot({ path: 'google-search.png' })
+    await browser.close();
+  }, 5000);
+
+  console.log("poonam");
+  } catch (error) {
+    console.log(`Error occurred: ${error.message}`)
+  }
 })();
